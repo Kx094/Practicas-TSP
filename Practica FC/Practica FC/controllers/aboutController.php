f76ed08afc0a5dc4bfdaae817b3ea89ffec97106
Kevin Saravia <131044145+Kx094@users.noreply.github.com>
2025-02-12 11:01:51 -0600

    Se añadió la practica del front controller
    Se añadieron los archivos de las vistas, los controladores y el index para la práctica de front controller

---
+++ Practica FC/Practica FC/controllers/aboutController.php
@@ -0,0 +1,17 @@
+<?php
+
+function render()
+{
+
+    $about = getAbout();
+
+    include_once("views/about.php");
+}
+
+function getAbout()
+{
+
+    $about = "<a href='?page=home'>Home</a>
+            <a href='?page=contact'>Contact</a>";
+    return $about;
+}
